Improved plotting and fixed inlet step computation

semiChannels.py
@@ -5,13 +5,14 @@ from functions import *
 # -------------------------
 
 # Model settings
-st       = 1  # Solid transport switch: 0=fixed bed; 1=movable bed
-Hcoeff   = -0.05  # =(Hd-H0)/D0, where H0 is the wse associated to D0 and Hd is the imposed downstream BC
-Qcoeff   = 1  # =Qa/Q0
-Qscoeff  = 1  # =Qsa/Qs0
+st       = 0  # Solid transport switch: 0=fixed bed; 1=movable bed
+Hcoeff   = 0  # =(Hd-H0)/D0, where H0 is the wse associated to D0 and Hd is the imposed downstream BC
+bedBC    = 'exp'  # available options: 'exp' adds a gradual inlet step with an exponential trend, that causes
+# deltaEta[-1]=inStep; 'flat'
+numPlots = 20
 
 # Numerical parameters
-dt      = 50  # timestep [s]
+dt      = 100  # timestep [s]
 dx      = 25  # cell length [m]
 tend    = 8
 maxIter = int(1e6)*st+1 # max number of iterations during time evolution
@@ -19,22 +20,19 @@ tol     = 1e-6 # Newton method tolerance
 
 # Hydraulic parameters
 RF     = 'ks' # flow resistance formula. Available options: 'ks' (Gauckler&Strickler), 'C' (Chézy)
-ks0    = 30 # if =0, it is computed using Gauckler&Strickler formula; otherwise it's considered a constant
+ks0    = 0 # if =0, it is computed using Gauckler&Strickler formula; otherwise it's considered a constant
 C0     = 0 # if =0, it is computed using a logarithmic formula; otherwise it's considered a constant
 eps_c  = 2.5 # Chézy logarithmic formula coefficient
 TF     = 'P90' # sediment transport formula. Available options: 'P78' (Parker78), 'MPM', 'P90' (Parker90), 'EH' (Engelund&Hansen)
 Ls     = 900 # =L/D0, L=branches' dimensional length
-beta0  = 15
-theta0 = 0.07
+beta0  = 20
+theta0 = 0.08
 ds0    = 0.01 # =d50/D0
 rW     = 0.5 # =Wb/W_a, where Wb=Wc and W_a=upstream channel width
 d50    = 0.01 # median sediment diameter [m]
 p      = 0.6 # bed porosity
 r      = 0.5 # Ikeda parameter
-
-# Bed profile plotting parameters & setup
-plotBedEvolution  = True
-numPlots          = 20
+inStep = d50
 
 # Physical constants
 delta = 1.65
@@ -45,12 +43,14 @@ g     = 9.81
 # ----------------------------------
 
 # Main channel IC
+if ks0 == 0:
+    ks0 = 21.1/(d50**(1/6))
 S0    = theta0*delta*ds0
 D0    = d50/ds0
 W_a   = beta0*2*D0
 phi00, phiD0, phiT0 = phis_scalar(theta0, TF, D0, d50)
-Q0    = uniFlowQ(RF, W_a, S0, D0, d50, g, ks0, C0, eps_c)*Qcoeff
-Qs0   = W_a*np.sqrt(g*delta*d50 ** 3)*phi00*Qscoeff
+Q0    = uniFlowQ(RF, W_a, S0, D0, d50, g, ks0, C0, eps_c)
+Qs0   = W_a*np.sqrt(g*delta*d50 ** 3)*phi00
 Fr0   = Q0/(W_a*D0*np.sqrt(g*D0))
 nc    = int(Ls*D0/dx)
 
@@ -70,6 +70,8 @@ nIter     = math.ceil(tend*Tf/dt)+1
 eta_a     = np.zeros((nIter,nc))
 eta_ab    = np.zeros((nIter,nc))
 eta_ac    = np.zeros((nIter,nc))
+eta_ab_i  = np.zeros(nc+1)
+eta_ac_i  = np.zeros(nc+1)
 eta_a_ic  = np.zeros(nc)
 eta_ab_ic = np.zeros(nc)
 eta_ac_ic = np.zeros(nc)
@@ -96,6 +98,9 @@ t:list = [0]
 # IC
 eta_ab[0,:]  = np.linspace(0,-S0*dx*(nc-1),num=len(eta_ab[0,:]))
 eta_ac[0,:]  = eta_ab[0,:]
+if bedBC == 'exp':
+    eta_ab[0,:]  -= inStep*np.exp(-(xc[-1]-xc[:])/(0.25*Ls*D0))
+    eta_ac[0,:]  += inStep*np.exp(-(xc[-1]-xc[:])/(0.25*Ls*D0))
 eta_a[0,:]   = (eta_ab[0,:]+eta_ac[0,:])/2
 eta_ab_ic[:] = eta_ab[0,:]
 eta_ac_ic[:] = eta_ac[0,:]
@@ -105,23 +110,36 @@ W_ac         = W_a/2
 Q_ab[:]      += Q0/2
 Q_ac[:]      += Q0/2
 
-# Downstream BC
-H0 = (eta_ab[0,-1]-S0*dx/2+D0)+D0*Hcoeff
+# Plot bed elevation IC
+eta_a_plot = np.vstack([eta_ac[0,:], eta_ab[0,:]])
+etamin     = min(np.amin(eta_a_plot), -np.amax(eta_a_plot))
+etamax     = max(np.amax(eta_a_plot), -np.amin(eta_a_plot))
+
+if bedBC=='exp':
+    myPlot(0,xc,eta_ab[0,:],'eta_ab')
+    myPlot(0,xc,eta_ac[0,:],'eta_ac')
+    plt.show()
 
 for n in range(0, maxIter):
     # Channel cells slope and inlet step update
-    S_ab[1:-1] = ((eta_ab[n,:-2]-eta_ab[n,1:-1])/dx+(eta_ab[n,1:-1]-eta_ab[n,2:])/dx)/2
-    S_ab[0]    = (eta_ab[n,0]- eta_ab[n,1])/dx
-    S_ab[-1]   = (eta_ab[n,-2]-eta_ab[n,-1])/dx
-    S_ac[1:-1] = ((eta_ac[n,:-2]-eta_ac[n,1:-1])/dx+(eta_ac[n,1:-1]-eta_ac[n,2:])/dx)/2
-    S_ac[0]    = (eta_ac[n,0]- eta_ac[n,1])/dx
-    S_ac[-1]   = (eta_ac[n,-2]-eta_ac[n,-1])/dx
-    deltaEta[n,:-1] = (eta_ab[n,:]-eta_ac[n,:])/D0
-    deltaEta[n,-1]  = deltaEta[n,-2]
+    S_ab[1:-1]      = ((eta_ab[n,:-2]-eta_ab[n,1:-1])/dx+(eta_ab[n,1:-1]-eta_ab[n,2:])/dx)/2
+    S_ab[0]         = (eta_ab[n,0]- eta_ab[n,1])/dx
+    S_ab[-1]        = (eta_ab[n,-2]-eta_ab[n,-1])/dx
+    S_ac[1:-1]      = ((eta_ac[n,:-2]-eta_ac[n,1:-1])/dx+(eta_ac[n,1:-1]-eta_ac[n,2:])/dx)/2
+    S_ac[0]         = (eta_ac[n,0]- eta_ac[n,1])/dx
+    S_ac[-1]        = (eta_ac[n,-2]-eta_ac[n,-1])/dx
+    eta_ab_i[:-1]   = eta_ab[n,:]+S_ab[:]*dx/2
+    eta_ac_i[:-1]   = eta_ac[n,:]+S_ac[:]*dx/2
+    eta_ab_i[-1]    = eta_ab[n,-1]-S_ab[-1]*dx/2
+    eta_ac_i[-1]    = eta_ac[n,-1]-S_ac[-1]*dx/2
+    deltaEta[n,:]   = (eta_ab_i[:]-eta_ac_i[:])/D0
 
     # Downstream BC: H(t)=H0
-    D_ab[-1] = H0-eta_ab[n,-1]+S_ab[-1]*dx/2
-    D_ac[-1] = H0-eta_ac[n,-1]+S_ac[-1]*dx/2
+    Q_ab[-1] = opt.fsolve(fSysLocalUnsteady, Q_ab[-1], (eta_ab_i[-1], eta_ac_i[-1], S_ab[-1],
+         S_ac[-1], W_ab, W_ac, Q0, g, d50, D0, RF, ks0, C0, eps_c), xtol=tol)[0]
+    Q_ac[-1] = Q0-Q_ab[-1]
+    D_ab[-1] = uniFlowD(RF, Q_ab[-1], W_ab, S_ab[-1], d50, g, ks0, C0, eps_c, D0)
+    D_ac[-1] = uniFlowD(RF, Q_ac[-1], W_ac, S_ac[-1], d50, g, ks0, C0, eps_c, D0)
     D_a[-1]  = (D_ab[-1]+D_ac[-1])/2
 
     # Solve the governing system to compute the unknowns D_ab[i], D_ac[i], Q_ab[i], Q_ac[i] and Qy[i]
@@ -142,9 +160,11 @@ for n in range(0, maxIter):
     Qs_ac    = W_ac*np.sqrt(g*delta*d50**3)*phis(Theta_ac, TF, D0, d50)[0]*st
     Qs_ab[0] = Qs0/2*st
     Qs_ac[0] = Qs0/2*st
+
     # Transverse solid discharge along channel a
     Theta_a = shieldsUpdate(RF, Q0, W_a, D_a, d50, g, delta, ks0, C0, eps_c)
     Qs_y[:] = (Qs_ab[:-1]+Qs_ac[:-1])*(Q_y/Q0-2*r*dx/(W_a*Theta_a[:-1]**0.5)*(eta_ab[n,:]-eta_ac[n,:])/W_a)
+    
     # Apply Exner equation to update node cells elevation
     eta_ab[n+1,:] = eta_ab[n,:] + dt*(Qs_ab[:-1]-Qs_ab[1:]+Qs_y[:])/((1-p)*W_ab*dx)*st
     eta_ac[n+1,:] = eta_ac[n,:] + dt*(Qs_ac[:-1]-Qs_ac[1:]-Qs_y[:])/((1-p)*W_ac*dx)*st
@@ -171,8 +191,8 @@ nFig = 0
 # Bed evolution plot
 if st==1:
     # Plot evolution of average elevation eta_a
-    crange     = np.linspace(0, 1, numPlots)
-    bed_colors = plt.cm.viridis(crange)
+    crange          = np.linspace(0, 1, numPlots)
+    bed_colors      = plt.cm.viridis(crange)
     plotTimeIndexes = np.linspace(0, n+1, numPlots)
     nFig += 1
     plt.figure(nFig)