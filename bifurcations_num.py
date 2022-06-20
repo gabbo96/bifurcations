48a4c631239bae51e637ee83e87337903a25e3a0
gabbo96 <gabriele.barile96@gmail.com>
2022-06-20 19:45:16 +0200

    - Removed channel a switch, now it's always modeled - Added confluence as possible downstream BC - D, S, theta, Q and Qs are now stored in a matrix at each timestep for the 3 branches - iterations now start at n=1 -> coherent with IC setting

--- bifurcations_num.py
+++ bifurcations_num.py
@@ -5,37 +5,42 @@ from functions import *
 # -------------------------
 
 # Model main settings
-channelAswitch = 1  # if=1, a 1D model of the upstream channel (as long as the branches) is added
+dsBC           = 1 # if 0, downstream BC is H0, possibly varying according to THd; if 1, the deltaH
+# imposed by a confluence is computed at each iteration according to the linear momentum cons. eqns of Ragno (2020)
 inStepIC       = -0.05  # =(inlet step at t=0)/(inlet step at equilibrium computed via BRT)
 alpha_var      = 'cost' # 'deltaEtaLin': alpha varies linearly with the inlet step
-THd            = -35 # =downstream BC timescale (the higher THd, the slower Hd varies over time)
+THd            = 0 # =downstream BC timescale (the higher THd, the slower Hd varies over time)
 
 # I/O settings
 numPlots    = 20
-showPlots   = True
 saveOutputs = False
 
 # Numerical parameters
 CFL       = 0.9 # Courant-Friedrichs-Lewy parameter for timestep computation
 dx        = 50 # cell length [m]
-tend      = 100
+tend      = 300
 tEq       = 4 # if ∆Q remains constant for tEq (non-dimensional), the simulation ends
 maxIter   = int(1e6) # max number of iterations during time evolution
 tol       = 1e-10 # Iterations tolerance
 
 # Hydraulic parameters
 RF     = 0 # flow resistance formula switch: if=0, C=C0 is a constant; if=1, C varies according to local water depth
-C0     = 0 # if =0, it's computed as a function of D0=ds0/d50 via a logarithmic formula
+C0     = 12 # if =0, it's computed as a function of D0=ds0/d50 via a logarithmic formula
 eps_c  = 2.5 # Chézy logarithmic formula coefficient (used only if RF=1 or C0=0)
 TF     = 'P90' # sediment transport formula. Available options: 'P78' (Parker78), 'MPM', 'P90' (Parker90), 'EH' (Engelund&Hansen)
-Ls     = 500 # =L/D0, L=branches' dimensional length
-beta0  = 17
+Ls     = 700 # =L/D0, L=branches' dimensional length
+beta0  = 17.5
 theta0 = 0.08
 ds0    = 0.01 # =d50/D0
 d50    = 0.01 # median sediment diameter [m]
 p      = 0.6 # bed porosity
 alpha  = 0  # if ==0, it is computed by assuming betaR=betaC
 r      = 0.5 # Ikeda parameter
+
+# Confluence parameters
+kI = 0.21
+kb = 0.08
+kc = 0.08
 
 # Physical constants
 delta = 1.65
@@ -83,48 +88,48 @@ print('\nINPUT PARAMETERS:\nFlow resistance formula = %s\nSolid transport formul
 print("BRT equilibrium solution:\n∆η = %5.4f" % inStep_eq_BRT)
 print("∆Q = %5.4f\nθ_b = %4.3f, θ_c = %4.3f\nFr_b = %3.2f, Fr_c = %3.2f\nS = %2.1e\n" % (BRT_out[:6]))
 
-# Arrays initialization
+# Arrays initialization to IC
 t         : list = [0]
+dts       : list = []  # list where to store the timestep values computed by means of CFL
 inStep    : list = [inStepIC*inStep_eq_BRT]
 deltaQ    : list = [0]
-dts       : list = []
-Qsc_ctrl  = np.ones((maxIter+1,nc+1))
 eta_bn    = np.zeros(maxIter+1)
 eta_cn    = np.zeros(maxIter+1)
 eta_a     = np.zeros((maxIter+1,nc+1))
 eta_b     = np.zeros((maxIter+1,nc+1))
 eta_c     = np.zeros((maxIter+1,nc+1))
-D_a       = np.ones(nc+1)*D0
-D_b       = np.ones(nc+1)*D0
-D_c       = np.ones(nc+1)*D0
-S_a       = np.ones(nc)*S0
-S_b       = np.ones(nc)*S0
-S_c       = np.ones(nc)*S0
-Theta_a   = np.ones(nc+1)*theta0
-Theta_b   = np.ones(nc+1)*theta0
-Theta_c   = np.ones(nc+1)*theta0
-Qs_b      = np.ones(nc+1)*Qs0/2
-Qs_c      = np.ones(nc+1)*Qs0/2
+D_a       = np.ones((maxIter+1,nc+1))*D0
+D_b       = np.ones((maxIter+1,nc+1))*D0
+D_c       = np.ones((maxIter+1,nc+1))*D0
+S_a       = np.ones((maxIter+1,nc))*S0
+S_b       = np.ones((maxIter+1,nc))*S0
+S_c       = np.ones((maxIter+1,nc))*S0
+Theta_a   = np.ones((maxIter+1,nc+1))*theta0
+Theta_b   = np.ones((maxIter+1,nc+1))*theta0
+Theta_c   = np.ones((maxIter+1,nc+1))*theta0
+Q_b       = np.ones(maxIter+1)*Q0/2
+Q_c       = np.ones(maxIter+1)*Q0/2
+Qs_a      = np.ones((maxIter+1,nc+1))*Qs0
+Qs_b      = np.ones((maxIter+1,nc+1))*Qs0/2
+Qs_c      = np.ones((maxIter+1,nc+1))*Qs0/2
+Qs_y      = np.zeros(maxIter+1)
+Hd_b      = np.zeros(maxIter+1)
+Hd_c      = np.zeros(maxIter+1)
 xi        = np.linspace(0,nc*dx,nc+1)  # cell interfaces coordinates
 
-# Branches IC. Horizontal step -> elevation of first cell of each branch is equal to the corresponding node cell
+# Bed elevation and width IC. Horizontal step -> elevation of first cell of each branch is equal to the corresponding node cell
 eta_bn[0]  =  inStep[0]*D0/2
 eta_cn[0]  = -inStep[0]*D0/2
+eta_a[0,:] = np.linspace(S0*dx*nc,0,num=nc+1)  # 0 is the mean bed elevation at the node, i.e., 0.5*(eta_bn+eta_cn)
 eta_b[0,:] = np.linspace(eta_bn[0], eta_bn[0]-S0*nc*dx, num=nc+1)
 eta_c[0,:] = np.linspace(eta_cn[0], eta_cn[0]-S0*nc*dx, num=nc+1)
 W_b = 0.5*W0
 W_c = 0.5*W0
-Q_b = Q0/2
-Q_c = Q0/2
-
-# Upstream channel IC
-if channelAswitch==1:
-    eta_a[0,:] = np.linspace(S0*dx*nc,0,num=nc+1)
 
 # Branches downstream BC: H(t)=H0
 H0 = 0.5*(eta_b[0,-1]+eta_c[0,-1])+D0
-Hd_b: list = [H0]
-Hd_c: list = [H0]
+Hd_b[0] = H0
+Hd_c[0] = H0
 
 # alpha time variation setup
 if alpha_var == 'cost':
@@ -134,25 +139,24 @@ else:
 
 # Time evolution
 eqReached = False
-for n in range(0, maxIter):
+for n in range(1, maxIter):
     # Compute dt according to CFL condition and update time. Check if system has reached equilibrium
-    Ceta_a = C_eta(Q0,W0,D_a,g,delta,d50,p,C0,D0,RF,TF,eps_c)
-    Ceta_b = C_eta(Q_b,W_b,D_b,g,delta,d50,p,C0,D0,RF,TF,eps_c)
-    Ceta_c = C_eta(Q_c,W_c,D_c,g,delta,d50,p,C0,D0,RF,TF,eps_c)
-    Cmax = max(max(Ceta_a),max(Ceta_b),max(Ceta_c))
-    dt   = CFL*dx/Cmax
+    Ceta_a = C_eta(Q0    ,W0 ,D_a[n,:],g,delta,d50,p,C0,D0,RF,TF,eps_c)
+    Ceta_b = C_eta(Q_b[n],W_b,D_b[n,:],g,delta,d50,p,C0,D0,RF,TF,eps_c)
+    Ceta_c = C_eta(Q_c[n],W_c,D_c[n,:],g,delta,d50,p,C0,D0,RF,TF,eps_c)
+    Cmax   = max(max(Ceta_a),max(Ceta_b),max(Ceta_c))
+    dt     = CFL*dx/Cmax
     dts.append(dt)
     t.append(t[-1]+dt)
-    # Time print
-    if n % 500 == 0:
-        print("Elapsed time = %4.1f Tf, ∆Q = %5.4f" % (t[n]/Tf,deltaQ[n]))
+    
     # Check for avulsion
     if abs(deltaQ[-1])>0.98:
         print("\nAvulsion occured\n")
         break
-    # Check if equilibrium or end of time have been reached
+
+    # Check if equilibrium or end of simulation time have been reached
     if t[-1]>tEq*Tf:
-        if np.all(abs(deltaQ[-int(tEq*Tf/dt):]/deltaQ[-1]-1)<np.sqrt(tol)):
+        if np.all(abs(deltaQ[-int(tEq*Tf/dt):]/(deltaQ[-1])-1)<np.sqrt(tol)):
             if THd == 0:
                 print('\nEquilibrium reached\n')
                 break
@@ -166,78 +170,86 @@ for n in range(0, maxIter):
         print('\nEnd time reached\n')
         break
 
-    # Update the downstream BC and use it to compute water depths at branches end
-    if eqReached and THd != 0:
-        Hd_b.append(Hd_b[-1]+D0/THd*dt/Tf)
-        Hd_c.append(Hd_c[-1]+D0/THd*dt/Tf)
-    D_b[-1] = Hd_b[-1]-eta_b[n,-1]
-    D_c[-1] = Hd_c[-1]-eta_c[n,-1]
+    # Update and impose the downstream BC
+    if dsBC == 0:
+        # Equal wse at downstream ends of branches b and c
+        if not eqReached:
+            Hd_b[n] = H0
+            Hd_c[n] = H0
+        elif eqReached and THd != 0:
+            Hd_b[n] = Hd_b[n-1]+D0/THd*dt/Tf
+            Hd_c[n] = Hd_c[n-1]+D0/THd*dt/Tf
+    elif dsBC == 1:
+        # deltaH set by a confluence
+        Hd_b[n],Hd_c[n] = deltaH_confluence(Q_b[n-1],Q_c[n-1],D_b[n-1,-1],D_c[n-1,-1],W_b,W_c,Q0,D0,W0,H0,kI,kb,kc,g)
+
+    D_b[n,-1] = Hd_b[n]-eta_b[n-1,-1]
+    D_c[n,-1] = Hd_c[n]-eta_c[n-1,-1]
     
-    # Compute the discharge partitioning at the node through profiles+Hb=Hc
-    D_b = buildProfile_rk4(RF,D_b[-1],Q_b,W_b,S_b,d50,dx,g,C0,eps_c)
-    D_c = buildProfile_rk4(RF,D_c[-1],Q_c,W_c,S_c,d50,dx,g,C0,eps_c)
-    if abs((D_b[0]-D_c[0]+inStep[-1]*D0)/(0.5*(D_b[0]+D_c[0])))>tol:
-        Q_b = opt.fsolve(fSys_rk4,Q_b,(RF,D_b[-1],D_c[-1],D0,inStep[-1],Q0,W_b,W_c,S_b,S_c,d50,dx,g,C0,eps_c),xtol=tol)
-        Q_c = Q0-Q_b
-        D_b = buildProfile_rk4(RF,D_b[-1],Q_b,W_b,S_b,d50,dx,g,C0,eps_c)
-        D_c = buildProfile_rk4(RF,D_c[-1],Q_c,W_c,S_c,d50,dx,g,C0,eps_c)
+    # Compute the discharge partitioning at the node through profiles+(Hb=Hc)
+    D_b[n,:] = buildProfile_rk4(RF,D_b[n,-1],Q_b[n],W_b,S_b[n-1,:],d50,dx,g,C0,eps_c)
+    D_c[n,:] = buildProfile_rk4(RF,D_c[n,-1],Q_c[n],W_c,S_c[n-1,:],d50,dx,g,C0,eps_c)
+    if abs((D_b[n,0]-D_c[n,0]+inStep[-1]*D0)/(0.5*(D_b[n,0]+D_c[n,0])))>tol:
+        Q_b[n]   = opt.fsolve(fSys_rk4,Q_b[n],(RF,D_b[n,-1],D_c[n,-1],D0,inStep[-1],Q0,W_b,W_c,S_b[n-1,:],S_c[n-1,:],d50,dx,g,C0,eps_c),xtol=tol)
+        Q_c[n]   = Q0-Q_b[n]
+        D_b[n,:] = buildProfile_rk4(RF,D_b[n,-1],Q_b[n],W_b,S_b[n-1,:],d50,dx,g,C0,eps_c)
+        D_c[n,:] = buildProfile_rk4(RF,D_c[n,-1],Q_c[n],W_c,S_c[n-1,:],d50,dx,g,C0,eps_c)
 
-    #Shields and Qs update for the anabranches
-    Theta_b = shieldsUpdate(RF,Q_b,W_b,D_b,d50,g,delta,C0,eps_c)
-    Theta_c = shieldsUpdate(RF,Q_c,W_c,D_c,d50,g,delta,C0,eps_c)
-    Qs_b    = W_b*np.sqrt(g*delta*d50**3)*phis(Theta_b,TF,D0,d50)[0]
-    Qs_c    = W_c*np.sqrt(g*delta*d50**3)*phis(Theta_c,TF,D0,d50)[0]
-    Qsc_ctrl[n,:] = Qs_c/(Qs0/2)
-
-    # Solve channel a
-    if channelAswitch == 1:
-        D_a[-1] = 0.5*(D_b[0]+D_c[0])+0.5*(eta_bn[n]+eta_cn[n])-eta_a[n,-1]
-        D_a     = buildProfile_rk4(RF,D_a[-1],Q0,W0,S_a,d50,dx,g,C0,eps_c)
-        Theta_a = shieldsUpdate(RF,Q0,W0,D_a,d50,g,delta,C0,eps_c)
-        Qs_a    = W0*np.sqrt(g*delta*d50**3)*phis(Theta_a,TF,D0,d50)[0]
+    # Computer water-surface profile along channel a
+    D_a[n,-1] = 0.5*(D_b[n,0]+D_c[n,0])+0.5*(eta_bn[n-1]+eta_cn[n-1])-eta_a[n-1,-1]
+    D_a[n, :] = buildProfile_rk4(RF,D_a[n,-1],Q0,W0,S_a[n-1,:],d50,dx,g,C0,eps_c)
+    
+    #Shields and Qs update for the three channels
+    Theta_a[n,:] = shieldsUpdate(RF,Q0    ,W0 ,D_a[n,:],d50,g,delta,C0,eps_c)
+    Theta_b[n,:] = shieldsUpdate(RF,Q_b[n],W_b,D_b[n,:],d50,g,delta,C0,eps_c)
+    Theta_c[n,:] = shieldsUpdate(RF,Q_c[n],W_c,D_c[n,:],d50,g,delta,C0,eps_c)
+    Qs_a[n,:]    = W0 *np.sqrt(g*delta*d50**3)*phis(Theta_a[n,:],TF,D0,d50)[0]
+    Qs_b[n,:]    = W_b*np.sqrt(g*delta*d50**3)*phis(Theta_b[n,:],TF,D0,d50)[0]
+    Qs_c[n,:]    = W_c*np.sqrt(g*delta*d50**3)*phis(Theta_c[n,:],TF,D0,d50)[0]
 
     # Compute liquid and solid discharge in transverse direction at the node; update bed elevation of node cells accordingly
-    Q_y  = Q_b-Q0/2
-    Qs_y = Qs0*(Q_y/Q0-2*alpha*r/np.sqrt(Theta_a[-1])*(eta_bn[n]-eta_cn[n])/W0)
-    eta_bn[n+1] = eta_bn[n]+dt*(Qs0/2-Qs_b[0]+Qs_y)/((1-p)*alpha*W0*W_b)
-    eta_cn[n+1] = eta_cn[n]+dt*(Qs0/2-Qs_c[0]-Qs_y)/((1-p)*alpha*W0*W_c)
+    Q_y       = 0.5*(Q_b[n]-Q_c[n])
+    Qs_y  [n] = Qs0*(Q_y/Q0-2*alpha*r/np.sqrt(Theta_a[n,-1])*(eta_bn[n-1]-eta_cn[n-1])/W0)
+    eta_bn[n] = eta_bn[n-1]+dt*(Qs0/2-Qs_b[n,0]+Qs_y[n])/((1-p)*alpha*W0*W_b)
+    eta_cn[n] = eta_cn[n-1]+dt*(Qs0/2-Qs_c[n,0]-Qs_y[n])/((1-p)*alpha*W0*W_c)
     
     # Update of bed elevation along the branches
-    eta_b[n+1,0] = eta_bn[n+1] # horizontal step
-    eta_c[n+1,0] = eta_cn[n+1]
-    eta_b[n+1,1:] = eta_b[n,1:]-dt/dx*(Qs_b[1:]-Qs_b[:-1])/(W_b*(1-p)) # upwind exner (Fr<1, so bed level perturbations travel downstream)
-    eta_c[n+1,1:] = eta_c[n,1:]-dt/dx*(Qs_c[1:]-Qs_c[:-1])/(W_c*(1-p))    
+    eta_a[n,0 ] = eta_a [n-1,0] -dt/dx*(Qs_a[n,1]-Qs0)/(W0*(1-p))
+    eta_a[n,1:] = eta_a [n-1,1:]-dt/dx*(Qs_a[n,1:]-Qs_a[n,:-1])/(W0*(1-p))
+    eta_b[n,0 ] = eta_bn[n] # horizontal step
+    eta_c[n,0 ] = eta_cn[n]
+    eta_b[n,1:] = eta_b [n-1,1:]-dt/dx*(Qs_b[n,1:]-Qs_b[n,:-1])/(W_b*(1-p)) # upwind exner (Fr<1, so bed level perturbations travel downstream)
+    eta_c[n,1:] = eta_c [n-1,1:]-dt/dx*(Qs_c[n,1:]-Qs_c[n,:-1])/(W_c*(1-p))    
 
     # Update bed slopes
-    S_b = (eta_b[n+1,:-1]-eta_b[n+1,1:])/dx
-    S_c = (eta_c[n+1,:-1]-eta_c[n+1,1:])/dx
-
-    # Update channel a
-    if channelAswitch == 1:
-        eta_a[n+1,0]  = eta_a[n,0]-dt/dx*(Qs_a[1]-Qs0)/(W0*(1-p))
-        eta_a[n+1,1:] = eta_a[n,1:]-dt/dx*(Qs_a[1:]-Qs_a[:-1])/(W0*(1-p))
-        S_a          = (eta_a[n+1,:-1]-eta_a[n+1,1:])/dx
+    S_a[n,:] = (eta_a[n,:-1]-eta_a[n,1:])/dx
+    S_b[n,:] = (eta_b[n,:-1]-eta_b[n,1:])/dx
+    S_c[n,:] = (eta_c[n,:-1]-eta_c[n,1:])/dx
 
     # Update time-controlled lists
-    deltaQ.append((Q_b-Q_c)/Q0)
-    inStep.append((eta_bn[n+1]-eta_cn[n+1])/D0)
+    deltaQ.append((Q_b[n]-Q_c[n])/Q0)
+    inStep.append((eta_bn[n]-eta_cn[n])/D0)
 
     # alpha update
     if alpha_var == 'deltaEtaLin':
         alpha = 0.1+(alpha_eq-0.1)*abs(inStep[-1]/inStep_eq_BRT)
+    
+    # Time print
+    if n % 500 == 0:
+        print("Elapsed time = %4.1f Tf, ∆Q = %5.4f" % (t[n]/Tf,deltaQ[n]))
 
 # Print final ∆Q
 print('Final ∆Q = %5.4f' % deltaQ[-1])
 print('∆Q at equilibrium according to BRT = %5.4f' % deltaQ_eq_BRT)
-print('Difference = %2.1f %%\n' % (100 * abs((abs(deltaQ[-1]) - deltaQ_eq_BRT) / deltaQ_eq_BRT)))
+print('Difference = %2.1f %%\n' % (100*abs((abs(deltaQ[-1])-deltaQ_eq_BRT)/deltaQ_eq_BRT)))
 
 # PLOTS
 # -----
 nFig = 0
 
 # Bed evolution plot
-plotTimeIndexes = np.linspace(0, n, numPlots)
-crange          = np.linspace(0, 1, numPlots)
+plotTimeIndexes = np.linspace(0, n-1, numPlots)
+crange          = np.linspace(0,   1, numPlots)
 bed_colors      = plt.cm.viridis(crange)
 plt.figure(nFig)
 plt.title('Branch A bed evolution in time')
@@ -260,18 +272,18 @@ nFig += 2
 
 # Plot evolution of node cells over time
 nFig += 1
-myPlot(nFig,t[:-1]/Tf,(eta_bn[:n+1]-0.5*(eta_bn[0]+eta_cn[0]))/D0,'Node cell B','Evolution of node cells elevation over time', 't/Tf [-]', '(η-η0)/D0 [-]')
-myPlot(nFig,t[:-1]/Tf,(eta_cn[:n+1]-0.5*(eta_bn[0]+eta_cn[0]))/D0,'Node cell C')
+myPlot(nFig,t[:-1]/Tf,(eta_bn[:n]-0.5*(eta_bn[0]+eta_cn[0]))/D0,'Node cell B','Evolution of node cells elevation over time', 't/Tf [-]', '(η-η0)/D0 [-]')
+myPlot(nFig,t[:-1]/Tf,(eta_cn[:n]-0.5*(eta_bn[0]+eta_cn[0]))/D0,'Node cell C')
 
 # Plot bed evolution at relevant cross-sections (upstream, middle, downstream)
 nFig += 1
 fig, ax = plt.subplots(1, 3, num=nFig)
-ax[0].plot(t[:-1]/Tf, (eta_b[:n+1,       1 ]-eta_b[0,       1 ])/D0, label='Branch B')
-ax[0].plot(t[:-1]/Tf, (eta_c[:n+1,       1 ]-eta_c[0,       1 ])/D0, label='Branch C')
-ax[1].plot(t[:-1]/Tf, (eta_b[:n+1,int(nc/2)]-eta_b[0,int(nc/2)])/D0, label='Branch B')
-ax[1].plot(t[:-1]/Tf, (eta_c[:n+1,int(nc/2)]-eta_c[0,int(nc/2)])/D0, label='Branch C')
-ax[2].plot(t[:-1]/Tf, (eta_b[:n+1,-      1 ]-eta_b[0,-      1 ])/D0, label='Branch B')
-ax[2].plot(t[:-1]/Tf, (eta_c[:n+1,-      1 ]-eta_c[0,-      1 ])/D0, label='Branch C')
+ax[0].plot(t[:-1]/Tf, (eta_b[:n,       1 ]-eta_b[0,       1 ])/D0, label='Branch B')
+ax[0].plot(t[:-1]/Tf, (eta_c[:n,       1 ]-eta_c[0,       1 ])/D0, label='Branch C')
+ax[1].plot(t[:-1]/Tf, (eta_b[:n,int(nc/2)]-eta_b[0,int(nc/2)])/D0, label='Branch B')
+ax[1].plot(t[:-1]/Tf, (eta_c[:n,int(nc/2)]-eta_c[0,int(nc/2)])/D0, label='Branch C')
+ax[2].plot(t[:-1]/Tf, (eta_b[:n,-      1 ]-eta_b[0,-      1 ])/D0, label='Branch B')
+ax[2].plot(t[:-1]/Tf, (eta_c[:n,-      1 ]-eta_c[0,-      1 ])/D0, label='Branch C')
 subplotsLayout(ax, ['t/Tf [-]', 't/Tf [-]', 't/Tf [-]'], ['(η-η0)/D0 [-]', None, None],
                ['upstream', 'Bed elevation vs time\n\nmiddle', 'downstream'])
 # Plot ∆Q evolution over time
@@ -281,9 +293,5 @@ myPlot(nFig,t[1:-1]/Tf,deltaQ[1:]/deltaQ_eq_BRT,None,'Discharge asymmetry vs tim
 # Plot ∆η evolution over time
 nFig += 1
 myPlot(nFig,t[:-1]/Tf,abs(inStep/inStep_eq_BRT),None,'Non-dimensional inlet step vs time','t/Tf [-]','|∆η/∆η_BRT| [-]')
-
-# Plot computed values of timesteps
-nFig += 1
-myPlot(nFig,np.indices((len(dts),1))[0],dts,None,'Computed timesteps along the simulation','Iteration #','dt [s]')
 
 plt.show()
